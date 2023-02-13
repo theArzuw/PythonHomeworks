dc84c1491c65e0c66d2effeca7913f3901c62ec8
Arzu <119745513+arzunuzx1@users.noreply.github.com>
2023-02-13 17:08:02 +0300

    lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,6 +1,15 @@
-exam1 = int(input("1. Sinav Notunu Giriniz:"))
-exam2 = int(input("2. Sinav Notunu Giriniz:"))
+exam1 = int(input("1. Enter Exam Grade:"))
+exam2 = int(input("2. Enter Exam Grade:"))
+exam3 = int(input("3. Enter Exam Grade:"))
+exam4 = int(input("4. Enter Exam Grade:"))
 
-average = (exam1+exam2)/2
+average = (exam1+exam2+exam3+exam4)/4
 
 print(average)
+
+
+
+midtermGrade = float(input("Please enter the midterm grade: "))* 0.2
+finalGrade = float(input("Please enter the final grade: "))*0.8
+
+print(midtermGrade + finalGrade/2)
